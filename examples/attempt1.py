ab230c52d9bcb2a3c2c5698b028318f8c2ea722e
Mahta Gholizadeh Ansari <mahtag2@illinoi.sedu>
2018-09-07 23:16:35 -0500

    Revert "making it test for multiple values"
    This reverts commit 5ff436a155b71ae36835af588dddddeb4b942921.

--- examples/attempt1.py
+++ examples/attempt1.py
@@ -33,113 +33,101 @@ xmin = np.array([0. + dx[0] / 2., 0. + dx[1] / 2., 0. + dx[2] / 2.])
 xmax = np.array([Lx - dx[0] / 2., Ly - dx[1] / 2., Lz - dx[2] / 2.])
 
 # covairance kernel and scale parameters
-Prior_std = np.logspace(-16, 0) #[1,.1,.01,.001,.0001,.00001]
-RR = np.logspace(-6,2)#[0,.5**3,.5**2,.5]
-errors = []
-import itertools
-allcombos = itertools.product(Prior_std, RR)
-for prior_std, R in allcombos:
-    print(prior_std, R)
-    try:
-        prior_cov_scale = np.array([0.0001, 0.0001, 0.0001])
-        
-        def kernel(r): return (prior_std ** 2) * np.exp(-r)
-        
-        # for plotting
-        x = np.linspace(0. + dx[0] / 2., Lx - dx[0] / 2., N[0])
-        y = np.linspace(0. + dx[1] / 2., Ly - dx[1] / 2., N[1])
-        XX, YY = np.meshgrid(x, y)
-        pts = np.hstack((XX.ravel()[:, np.newaxis], YY.ravel()[:, np.newaxis]))
-        
-        inputDirectory = '/Users/mahtag2/syn_inverse_guess/'
-        inputFileName = '2DCr.in'
-        
-        directory= '/Users/mahtag2/syn_inverse_test' #directory of the Crunch ouptuts
-        perm=pyCrunch.getPerm(directory)
-        #
-        #permeability_x=perm2array(perm[0])[:,3]
-        #permeability_y=perm2array(perm[0])[:,4]
-        
-        s_true= pyCrunch.S2array(pyCrunch.getS(directory)[0])
-        #!s_true = sss.reshape(-1, 1)  # make it 2D array
-        
-         
-        brk= pyCrunch.getBreakthroughFiles(directory)
-        
-        obs_true=[] #making an empty list of mean travel times
-        #we usually have multiple brk files so we need to bring them all in!
-        for file in brk:
-            
-            obs_true.append(pyCrunch.getMeanTravelTime(pyCrunch.brk2array(file)))
-            
-        obs_true=np.array(obs_true)
-        
-        
-        
-        def forward_model(s_true,par,ncores = None):
-            if par == True:
-                raise ValueError("This crunchflow forward model does not support parallelization.")
-            inputDirectory = '/Users/mahtag2/syn_inverse_guess/'
-            inputFileName = '2DCr.in'
-            directory= '/Users/mahtag2/syn_inverse_test' #directory of the Crunch ouptuts
-            # Update make a new permeability files from s before running crunch
-            pyCrunch.runCrunch(inputDirectory,inputFileName)
-            bk= pyCrunch.getBreakthroughFiles(directory)
-        
-            obs_s=[] #making an empty list of mean travel times
-        # #we usually have mulptiple brk files so we need to bring them all in!
-            for file in bk:
-                obs_s.append(pyCrunch.getMeanTravelTime(pyCrunch.brk2array(file)))
-            simul_obs=np.array(obs_s)
-            
-            
-            Old_s=pyCrunch.getS(directory)
-            assert len(Old_s) == 1
-            k = pyCrunch.S2array(Old_s[0])
-            np.savetxt(os.path.join(directory,'PermField.x'),k)
-            return simul_obs
-        
-        
-        ##s_init = np.copy(s_true) # you can try with s_true!
-        ##s_init = s_init.reshape((s_init.shape[0],1))
-            
-        s_true = s_true.reshape((s_true.shape[0],1))
-        s_init = np.ones((m, 1))
-        
-        
-        params = {'R': R, 'n_pc': 5,
-                  'maxiter': 10, 'restol': 0.01,
-                  'matvec': 'FFT', 'xmin': xmin, 'xmax': xmax, 'N': N,
-                  'prior_std': prior_std, 'prior_cov_scale': prior_cov_scale,
-                  'kernel': kernel, 'post_cov': "diag",
-                  'precond': True, 'LM': True,
-                  'parallel': False, 'linesearch': True,
-                  'forward_model_verbose': False, 'verbose': False,
-                  'iter_save': True}
-        
-        ## initialize
-        pcga = PCGA(forward_model, s_init, pts, params, s_true, obs_true)
-        #out = pcga.CreateSyntheticData (s_true, noise = False)
-        
-        
-        
-        ## run inversion
-        #input("Press enter to continue")
-        s_hat, simul_obs, post_diagv, iter_best = pcga.Run() 
-        #
-        #s_hat3d = s_hat.reshape(nlay, nrow, ncol)
-        #s_hat2d = s_hat3d[0,:,:]
-        #s_true3d = s_true.reshape(nlay, nrow, ncol)
-        #s_true2d = s_true3d[0,:,:]
-        
-        #post_diagv[post_diagv < 0.] = 0.  # just in case we have stuff that is negavtive
-        ## it is a way for selecting stuff in the array that are smaller than zero
-        #post_std = np.sqrt(post_diagv)
-        #post_std3d = post_std.reshape(nlay, nrow, ncol)
-        #post_std2d = post_std3d[0,:,:]
-        errors.append((prior_std,R,'Yay'))
-    except Exception as e:
-        errors.append((prior_std,R,e))
+prior_std =0.0001
+prior_cov_scale = np.array([0.0001, 0.0001, 0.0001])
+
+def kernel(r): return (prior_std ** 2) * np.exp(-r)
+
+# for plotting
+x = np.linspace(0. + dx[0] / 2., Lx - dx[0] / 2., N[0])
+y = np.linspace(0. + dx[1] / 2., Ly - dx[1] / 2., N[1])
+XX, YY = np.meshgrid(x, y)
+pts = np.hstack((XX.ravel()[:, np.newaxis], YY.ravel()[:, np.newaxis]))
+
+inputDirectory = '/Users/mahtag2/syn_inverse_guess/'
+inputFileName = '2DCr.in'
+
+directory= '/Users/mahtag2/syn_inverse_test' #directory of the Crunch ouptuts
+perm=pyCrunch.getPerm(directory)
+#
+#permeability_x=perm2array(perm[0])[:,3]
+#permeability_y=perm2array(perm[0])[:,4]
+
+s_true= pyCrunch.S2array(pyCrunch.getS(directory)[0])
+#!s_true = sss.reshape(-1, 1)  # make it 2D array
+
+ 
+brk= pyCrunch.getBreakthroughFiles(directory)
+
+obs_true=[] #making an empty list of mean travel times
+#we usually have multiple brk files so we need to bring them all in!
+for file in brk:
     
-            
+    obs_true.append(pyCrunch.getMeanTravelTime(pyCrunch.brk2array(file)))
+    
+obs_true=np.array(obs_true)
+
+
+
+def forward_model(s_true,par,ncores = None):
+    if par == True:
+        raise ValueError("This crunchflow forward model does not support parallelization.")
+    inputDirectory = '/Users/mahtag2/syn_inverse_guess/'
+    inputFileName = '2DCr.in'
+    directory= '/Users/mahtag2/syn_inverse_test' #directory of the Crunch ouptuts
+    # Update make a new permeability files from s before running crunch
+    pyCrunch.runCrunch(inputDirectory,inputFileName)
+    bk= pyCrunch.getBreakthroughFiles(directory)
+
+    obs_s=[] #making an empty list of mean travel times
+# #we usually have mulptiple brk files so we need to bring them all in!
+    for file in bk:
+        obs_s.append(pyCrunch.getMeanTravelTime(pyCrunch.brk2array(file)))
+    simul_obs=np.array(obs_s)
+    
+    
+    Old_s=pyCrunch.getS(directory)
+    assert len(Old_s) == 1
+    k = pyCrunch.S2array(Old_s[0])
+    np.savetxt(os.path.join(directory,'PermField.x'),k)
+    return simul_obs
+
+
+##s_init = np.copy(s_true) # you can try with s_true!
+##s_init = s_init.reshape((s_init.shape[0],1))
+    
+s_true = s_true.reshape((s_true.shape[0],1))
+s_init = np.ones((m, 1))
+
+
+params = {'R': (0.5) ** 10, 'n_pc': 5,
+          'maxiter': 10, 'restol': 0.01,
+          'matvec': 'FFT', 'xmin': xmin, 'xmax': xmax, 'N': N,
+          'prior_std': prior_std, 'prior_cov_scale': prior_cov_scale,
+          'kernel': kernel, 'post_cov': "diag",
+          'precond': True, 'LM': True,
+          'parallel': False, 'linesearch': True,
+          'forward_model_verbose': False, 'verbose': False,
+          'iter_save': True}
+
+## initialize
+pcga = PCGA(forward_model, s_init, pts, params, s_true, obs_true)
+#out = pcga.CreateSyntheticData (s_true, noise = False)
+
+
+
+## run inversion
+#input("Press enter to continue")
+s_hat, simul_obs, post_diagv, iter_best = pcga.Run() 
+#
+#s_hat3d = s_hat.reshape(nlay, nrow, ncol)
+#s_hat2d = s_hat3d[0,:,:]
+#s_true3d = s_true.reshape(nlay, nrow, ncol)
+#s_true2d = s_true3d[0,:,:]
+
+#post_diagv[post_diagv < 0.] = 0.  # just in case we have stuff that is negavtive
+## it is a way for selecting stuff in the array that are smaller than zero
+#post_std = np.sqrt(post_diagv)
+#post_std3d = post_std.reshape(nlay, nrow, ncol)
+#post_std2d = post_std3d[0,:,:]
 
