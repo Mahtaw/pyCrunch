63106626edb7f6f54abb8f70801bf314d6f2d2f6
Mahta Gholizadeh Ansari <mahtag2@illinoi.sedu>
2018-09-15 15:54:58 -0500

    consolidating functions.

--- utilityFuncs.py
+++ utilityFuncs.py
@@ -23,15 +23,25 @@ def getBreakthroughFiles(directory):
     #this function cuts "breakthrough" and ".out" from all the names in brk list
     #int makes everything into integer! 
     brk.sort(key = sorterFunc) #here we are sorting the brk list according to sorterfunction values
-
     return brk
 
 #converting the files to array
 def brk2array(fileName):
     return np.genfromtxt(fileName,delimiter = [27,23],skip_header=2)# works like importer function in MATLab
 
-
-
+def getMeanTravelTimes(directory):
+    '''Get and sort the list of the breakthrough files in the working directory'''
+    brk = getBreakthroughFiles(directory)
+    '''Generate arrays from the breakthrough files'''
+    arrs = []
+    for file in brk:
+        arrs.append(brk2array(file))
+    '''Generate mean travel times from the arrays'''
+    obs = []
+    for arr in arrs:
+        obs.append(getMeanTravelTime(arr))
+    return np.array(obs)
+    
 '''Given a numpy array generated from the brk2array function, this function will return the mean travel time.'''
 def getMeanTravelTime(array):
     #Since the values in brk files are seperated with spaces we need to count the characters in between the columns! It is called 
@@ -50,41 +60,18 @@ def getPerm(directory):
     perm.sort(key = sorterFunc)
     return perm
 
-def getS(directory):
-    
-    old_s=glob(os.path.join(directory, 'PermField.x'))
+def getS(directory):   
+    files=glob(os.path.join(directory, 'PermField.x'))
+    assert len(files) == 1
+    s = np.genfromtxt(files[0])
+    s = s.reshape((s.shape[0],1))
+    return s
 
-    return old_s
-
-def S2array(filename):
-    return np.genfromtxt(filename)
     
 
 #making the permeability files into an array
 def perm2array(fileName):
-    return np.genfromtxt(fileName,delimiter = [20,17,16,17, 17],skip_header=3)
-
-
-    #running Crunch
-def runCrunch(inputFilePath, inputFileName, libraryPath = '/Users/mahtag2/Desktop/CrunchTope-InstallMac/libs/'):
-
-    crunchPath = os.path.join(libraryPath,'CrunchTope-Mac') #Actual directory of Crunch executable
-    #here were combining all the directories 
-    cmd = 'export DYLD_LIBRARY_PATH=' + libraryPath + '&&' + crunchPath + ' ' + os.path.join(inputFilePath, inputFileName)
-    #'export DYLD_LIBRARY_PATH=' works as source .bashrc in terminal. It is needed so that crunch can find the other libraries it needs.
-    # && combines the two commmands on a single line. Add DYLD libraries to the environment and then running crunch with the name of the input file
-    currentDirectory = os.getcwd()
-    os.chdir(inputFilePath)
-    proc = subprocess.Popen(cmd, shell=True, stdout = subprocess.PIPE, stderr= subprocess.PIPE)# subprocess execute the above command.
-    # stdout gets the output from the run, stderr gets the possible errors.
-    msg = proc.communicate() #communicate is when we need to see the outputs!
-    #I took out the timeout=10 because Crunch would take much longer 
-    os.chdir(currentDirectory)
-    print(msg[0].decode())#to make the output look like terminal output
-    print(msg[1].decode())#output possible errors
-   
-    
-    
+    return np.genfromtxt(fileName,delimiter = [20,17,16,17, 17],skip_header=3)           
     
     #Creating new input files based on a template
 def generateInputFile(outputPath,templatePath, d):#output path: directory of the new file, Templatepth: directory of the template
@@ -111,12 +98,14 @@ def generateInputFile(outputPath,templatePath, d):#output path: directory of the
             newline = str.join(' ',newline) #this will add back the white spaces to the line so that crunch could read it.      
             print(newline)#this will actually overwrite the values in the input file
 
-#def Generate
-        
+def generatePermeabilityFile(s: np.ndarray, outputPath: str) -> None:
+    path = os.path.join(outputPath, "PermField.x")
+    s = s.squeeze()
+    assert len(s.shape) == 1
+    np.savetxt(path,s)
         
   #Copying all the .dbs files needed to a new folder so that Crunch could run in it!          
-def dbsPopulater (outputPath,templatePath):
-     
+def dbsPopulater (outputPath,templatePath):  
     folders_src=glob(templatePath+'*.dbs')
     fileNames = [os.path.split(i)[-1] for i in folders_src]
     for i in fileNames:
@@ -126,6 +115,40 @@ def dbsPopulater (outputPath,templatePath):
         else:
             print("it already exists!")
             
-            
+class CrunchRun:
+    def __init__(self, initialInputDirectory, inputFileName, workingDirectory, libraryPath ):
+        if os.path.exists(workingDirectory):
+            if input("The working directory: {} already exists. Do you want to overwrite? (y/n)".format(workingDirectory)).strip().lower() == 'y':
+                shutil.rmtree(workingDirectory)
+            else:
+                raise RuntimeError("Execution Cancelled")
+        shutil.copytree(initialInputDirectory, workingDirectory)
+        self.inputFileName = inputFileName
+        self.libraryPath = libraryPath
+        self.workingDirectory = workingDirectory
+
+    def run(self, verbose = False):
+        if self.libraryPath is None:
+            raise ValueError("libraryPath has not been specified.")
+        if self.workingDirectory is None:
+            raise ValueError("The working directory has not been initialized.")
+        crunchPath = os.path.join(self.libraryPath,'CrunchTope-Mac') #Actual directory of Crunch executable
+        #here were combining all the directories 
+        cmd = 'export DYLD_LIBRARY_PATH=' + self.libraryPath + '&&' + crunchPath + ' ' + os.path.join(self.workingDirectory, self.inputFileName)
+        #'export DYLD_LIBRARY_PATH=' works as source .bashrc in terminal. It is needed so that crunch can find the other libraries it needs.
+        # && combines the two commmands on a single line. Add DYLD libraries to the environment and then running crunch with the name of the input file
+        currentDirectory = os.getcwd()
+        os.chdir(self.workingDirectory)
+        proc = subprocess.Popen(cmd, shell=True, stdout = subprocess.PIPE, stderr= subprocess.PIPE)# subprocess execute the above command.
+        # stdout gets the output from the run, stderr gets the possible errors.
+        msg = proc.communicate() #communicate is when we need to see the outputs! This blocks until crunchflow completes
+        #I took out the timeout=10 because Crunch would take much longer 
+        os.chdir(currentDirectory)
+        if verbose:
+            print(msg[0].decode())#to make the output look like terminal output
+        if msg[1] != b'':
+            raise RuntimeError("Crunchflow returned the following error: ", msg[1].decode())
+            #importing brk files,it is important to have them sorted
+
 
        
